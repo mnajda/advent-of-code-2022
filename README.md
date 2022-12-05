ea3188431f4537c9152182674ffd09ebd27b69a4
Mateusz Najda <mateusznajda@gmail.com>
2022-12-05 10:05:10 +0100

    added day5

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Solutions for [Advent of Code 2022](https://adventofcode.com/2022).
 | 2 | [Rock Paper Scissors](https://adventofcode.com/2022/day/2) | Rust | [day2](https://github.com/mnajda/advent-of-code-2022/tree/main/day2) |
 | 3 | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) | F# | [day3](https://github.com/mnajda/advent-of-code-2022/tree/main/day3) |
 | 4 | [Camp Cleanup](https://adventofcode.com/2022/day/4) | Python | [day4](https://github.com/mnajda/advent-of-code-2022/tree/main/day4) |
+| 5 | [Supply Stacks](https://adventofcode.com/2022/day/5) | Go | [day5](https://github.com/mnajda/advent-of-code-2022/tree/main/day5) |
---
+++ day5/part1/part1.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func MakeStack(raw_stacks []string) (stacks [][]byte) {
+	num_of_stacks := int(raw_stacks[len(raw_stacks)-1][len(raw_stacks[len(raw_stacks)-1])-2] - '0')
+	stacks = make([][]byte, num_of_stacks)
+
+	for i := range raw_stacks[:len(raw_stacks)-1] {
+		stack := raw_stacks[i]
+
+		stack_number := 0
+		for k := 1; k < len(stack); k += 4 {
+			if stack[k] == ' ' {
+				stack_number++
+				continue
+			}
+
+			stacks[stack_number] = append(stacks[stack_number], stack[k])
+			stack_number++
+		}
+	}
+
+	return
+}
+
+func MakeInstructions(raw_instructions []string) (instructions [][]int) {
+	instructions = make([][]int, len(raw_instructions))
+
+	for i := range raw_instructions {
+		raw_instruction := strings.Split(raw_instructions[i], " ")
+
+		for k := range raw_instruction {
+			if num, err := strconv.Atoi(raw_instruction[k]); err == nil {
+				instructions[i] = append(instructions[i], num)
+			}
+		}
+	}
+
+	return
+}
+
+func Load(path string) (stacks [][]byte, instructions [][]int) {
+	file, _ := os.Open(path)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	raw_stacks := make([]string, 0)
+	raw_instructions := make([]string, 0)
+
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			break
+		}
+
+		raw_stacks = append(raw_stacks, scanner.Text())
+	}
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		raw_instructions = append(raw_instructions, line)
+	}
+
+	stacks = MakeStack(raw_stacks)
+	instructions = MakeInstructions(raw_instructions)
+
+	return
+}
+
+func main() {
+	path := os.Args[1]
+
+	stacks, instructions := Load(path)
+
+	for i := range instructions {
+		instruction := instructions[i]
+
+		how_many := instruction[0]
+		from := instruction[1] - 1
+		to := instruction[2] - 1
+
+		for k := 0; k < how_many; k++ {
+			to_move := stacks[from][0]
+			stacks[to] = append([]byte{to_move}, stacks[to]...)
+			stacks[from] = stacks[from][1:]
+		}
+	}
+
+	result := make([]byte, 0)
+	for i := range stacks {
+		result = append(result, stacks[i][0])
+	}
+
+	fmt.Println(string(result))
+}
